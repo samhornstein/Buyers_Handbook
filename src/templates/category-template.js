8a519ffe33bee161c46db64f3f3d7896a4b669e5
Sam Hornstein <samhornstein@gmail.com>
2021-02-02 14:55:22 -0800

    Add dynamic title to category pages

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -1,19 +1,59 @@
 import React from 'react'
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const CategoryTemplate = ({ data, location }) => {
   const nodes = data.allMarkdownRemark.nodes
   const siteTitle = data.site.siteMetadata?.title || `Title`
 
   return (
-    <Layout title={siteTitle} location={location}>
-      <h3>{nodes[0].frontmatter.category}</h3>
-      {nodes.map(node => {
-        return(<p>{node.frontmatter.title}</p>)
-      })}
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Reviews" />
+      <h1>{nodes[0].frontmatter.category}</h1>
+      <ol style={{ listStyle: `none` }}>
+        {nodes.map(node => {
+          const title = node.frontmatter.title || node.fields.slug
+
+          return (
+            <li key={node.fields.slug}>
+              <article
+                className="node-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={node.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{node.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
+
+  // return (
+  //   <Layout title={siteTitle} location={location}>
+  //     <h3>{nodes[0].frontmatter.category}</h3>
+  //     {nodes.map(node => {
+  //       return(<p>{node.frontmatter.title}</p>)
+  //     })}
+  //   </Layout>
+  // )
 }
 
 export default CategoryTemplate
