533282ea269b61d6f4001957428937e66b369746
Sam Hornstein <samhornstein@gmail.com>
2021-02-16 11:08:47 -0800

    Remove succulents review

--- utils/main.py
+++ utils/main.py
@@ -4,10 +4,10 @@ from obtain_unsplash_image import obtain_unsplash_image
 
 # keyword = input('Please enter the keyword you would like to generate a review for: ')
 
-keyword = 'basketballs'
+keyword = 'succulents'
 
 df = store(keyword)
 
 obtain_unsplash_image(keyword)
 
-generate(keyword, author='Kenji Stinson', category='Sports', description='The Best Basketballs', special_status='Trending')
+generate(keyword, category='Garden', special_status='Trending')
