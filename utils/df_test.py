ccf684030df1aa9d7715d30ffaf2280f2e0870ac
Sam Hornstein <samhornstein@gmail.com>
2021-02-16 12:51:07 -0800

    delete df_test

--- utils/df_test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Users/samhornstein/gatsby-starter-blog-2/content/reviews/ice cream/df_output.csv')
-
-df.drop_duplicates(subset=['Title'], inplace=True)
-
-print(df.head())
